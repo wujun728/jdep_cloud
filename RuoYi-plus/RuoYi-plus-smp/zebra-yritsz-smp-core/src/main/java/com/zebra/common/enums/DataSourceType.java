965c6e390f366829e4b919d47b2ec15a3c03abc1
wujun728 <wujun728@hotmail.com>
2020-11-25 23:55:06 +0800

    1

---
+++ RuoYi-plus/RuoYi-plus-smp/zebra-yritsz-smp-core/src/main/java/com/zebra/common/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.zebra.common.enums;
+
+/**
+ * 数据源
+ * 
+ * @author Wujun
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
---
+++ RuoYi-plus/RuoYi-plus-smp/zebra-yritsz-smp-core/src/main/java/com/zebra/common/exception/LimitIpException.java
@@ -0,0 +1,14 @@
+package com.zebra.common.exception;
+
+/**
+ * ip限制异常
+ *
+ * @author Wujun
+ */
+public class LimitIpException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public LimitIpException() {
+
+	}
+}
